70236c023b0e80677f0df29aa0358c5a0cf400d4
coder8820 <kumail112113@gmail.com>
2025-09-02 23:54:15 -0700

    Added button and dummy content

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -3,12 +3,26 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log("Hello, Next.js!");
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline"
+        <h1 className="text-2xl font-bold underline"
         onClick={() => alert('Hello, Next.js!')}
         >Hello, Next.js!</h1>
+        <p className="mt-4 text-gray-400 max-w-md text-justify leading-relaxed">
+          lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          quod, quia, voluptatum, voluptate quibusdam voluptates quae
+          voluptatibus quas quibusdam. Quisquam, quod, quia, voluptatum,
+          voluptate quibusdam voluptates quae voluptatibus quas quibusdam.
+          Quisquam, quod, quia, voluptatum, voluptate quibusdam voluptates quae
+          voluptatibus quas quibusdam.
+        </p>
+        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        onClick={() => alert('Button Clicked!')}
+        >
+          Click Me
+        </button>
       </div>
     </>
   );
