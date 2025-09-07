b596b97e90266b802064119a76c5138480fbca95
coder8820 <kumail112113@gmail.com>
2025-09-07 08:56:04 -0700

    learning routing in nextjs which is very easy

---
+++ my-app/app/about/page.js
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <>
+            <div className="mx-auto p-4">
+                <h1>About Us</h1>
+                <p>This is the about page of our Next.js application.</p>
+            </div>
+        </>
+    )
+}
