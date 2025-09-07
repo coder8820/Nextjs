af0573c7c445d4848d83450f485ef9b04cfdd66c
coder8820 <kumail112113@gmail.com>
2025-09-07 08:46:05 -0700

    some important changes

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,28 +1,14 @@
 "use client";
 
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
-  console.log("Hello, Next.js!");
   return (
     <>
-      <div>
-        <h1 className="text-2xl font-bold underline"
-        onClick={() => alert('Hello, Next.js!')}
-        >Hello, Next.js!</h1>
-        <p className="mt-4 text-gray-400 max-w-md text-justify leading-relaxed">
-          This is a sample Next.js application demonstrating the use of Tailwind CSS for styling and interactivity with React components.
-        </p>
-        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer"
-        onClick={() => alert('Button Clicked!')}
-        >
-          Click Me
-        </button>
-        <div className="mt-6">
-          this is an image from public folder
-          <p>Helo world</p>
-        </div>
-       </div>
+      <div className="mx-auto p-4">
+        <h1>Welcome to Next.js!</h1>
+        <p>In the name of allah the most benificent and the most merciful</p>
+      </div>
     </>
   );
 }
