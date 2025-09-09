9d0a5877f098dfc812b8f92a0f4fbfdd1170317b
coder8820 <kumail112113@gmail.com>
2025-09-09 10:16:24 -0700

    some method not working

--- my-app/app/blogs/blogID/page.js
+++ my-app/app/blogs/blogID/page.js
@@ -1,10 +1,18 @@
-export default async function Blog1({ params }) {
-  const { blogID } = await params;
+const blogData = {
+  test: "This is test blog",
+  helo: "This is helo blog",
+  demo: "This is demo blog",
+};
+
+// app/blogs/[blogID]/page.js
+export default function BlogPage({ params }) {
+  const { blogID } = params;
   console.log(blogID);
+
   return (
-    <>
-      <h1>Blog 1 {blogID}</h1>
-      <p>This is the content of blog 1.</p>
-    </>
+    <div>
+      <h1>Blog {blogID}</h1>
+      <p>This is the content of blog {blogContent}.</p>
+    </div>
   );
 }
