80dd8ad70b847e7d5036539a0a48ce7c8148d969
coder8820 <kumail112113@gmail.com>
2025-09-09 09:21:24 -0700

    dynamic routing practices

--- my-app/app/blogs/blog/page.js
+++
@@ -1,9 +0,0 @@
-export default async function Page1({ params }) {
-  console.log(await params);
-  return (
-    <>
-      <h1>Blog 1</h1>
-      <p>This is the content of blog 1.</p>
-    </>
-  );
-}
---
+++ my-app/app/blogs/blogID/page.js
@@ -0,0 +1,13 @@
+async function Blog1({ params }) {
+  console.log(await params);
+  const { blogID } = await params.blogID;
+  console.log(blogID);
+  return (
+    <>
+      <h1>Blog 1 {blogID}</h1>
+      <p>This is the content of blog 1.</p>
+    </>
+  );
+}
+
+export default Blog1;
