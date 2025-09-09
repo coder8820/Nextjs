33d92218f51ccfacf5563df350a337ace8b350b2
coder8820 <kumail112113@gmail.com>
2025-09-09 09:26:49 -0700

    new changes in blog page1;

--- my-app/app/blogs/blogID/page.js
+++ my-app/app/blogs/blogID/page.js
@@ -1,13 +1,11 @@
-async function Blog1({ params }) {
+export default async function Blog1({ params }) {
   console.log(await params);
   const { blogID } = await params.blogID;
   console.log(blogID);
   return (
     <>
       <h1>Blog 1 {blogID}</h1>
-      <p>This is the content of blog 1.</p>
+      {/* <p>This is the content of blog 1.</p> */}
     </>
   );
 }
-
-export default Blog1;
