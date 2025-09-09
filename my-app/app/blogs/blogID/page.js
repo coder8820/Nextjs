fd3ddb4b7e214bd69e009607b6cc1d63b22a0225
coder8820 <kumail112113@gmail.com>
2025-09-09 09:50:27 -0700

    some bugs can not fixing

--- my-app/app/blogs/blogID/page.js
+++ my-app/app/blogs/blogID/page.js
@@ -1,11 +1,10 @@
 export default async function Blog1({ params }) {
-  console.log(await params);
-  const { blogID } = await params.blogID;
+  const { blogID } = await params;
   console.log(blogID);
   return (
     <>
       <h1>Blog 1 {blogID}</h1>
-      {/* <p>This is the content of blog 1.</p> */}
+      <p>This is the content of blog 1.</p>
     </>
   );
 }
