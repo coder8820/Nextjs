4b4ec3660d26b9e4dd677b96061984602e061ab1
coder8820 <kumail112113@gmail.com>
2025-09-10 08:51:42 -0700

    some comments

--- my-app/app/blogs/blogID/coments/page.js
+++ my-app/app/blogs/blogID/coments/page.js
@@ -5,6 +5,10 @@ export default function Commnets({ params }) {
     <div>
       <h1>Comment 1</h1>
       <h2>All commnets on {blogID}</h2>
+      <h3>Comment 2</h3>
+      <h4>Comment 3</h4>
+      <h5>Comment 4</h5>
+      <h6>Comment 5</h6>
     </div>
   );
 }
