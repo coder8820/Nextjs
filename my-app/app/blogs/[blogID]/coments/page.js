825f61d0e7cd337b34485e8cdad32ab3062f24a5
coder8820 <kumail112113@gmail.com>
2025-09-10 09:16:58 -0700

    now understand nested routing

--- my-app/app/blogs/[blogID]/coments/page.js
+++ my-app/app/blogs/[blogID]/coments/page.js
@@ -3,12 +3,8 @@ export default function Commnets({ params }) {
   console.log(blogID);
   return (
     <div>
-      <h1>Comment 1</h1>
+      <h1>Comment {blogID}</h1>
       <h2>All commnets on {blogID}</h2>
-      <h3>Comment 2</h3>
-      <h4>Comment 3</h4>
-      <h5>Comment 4</h5>
-      <h6>Comment 5</h6>
     </div>
   );
 }
