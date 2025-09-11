a7e5554378a9f046f71a26145859195c98aaeeac
coder8820 <kumail112113@gmail.com>
2025-09-10 23:13:24 -0700

    integrated new lines of code

--- my-app/app/blogs/[blogID]/coments/[comment]/page.js
+++ my-app/app/blogs/[blogID]/coments/[comment]/page.js
@@ -10,6 +10,7 @@ export default async function comment({ params }) {
         Comment No, <i>{comment}</i> on <b>{blogID} page</b>
       </p>
       <p>In next js file base routing can get all parent data</p>
+      <p>Nested Routing is very easy than react router.</p>
     </div>
   );
 }
