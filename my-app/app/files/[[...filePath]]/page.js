17b987591364f7c6d6426c7bf3d533964692f213
coder8820 <kumail112113@gmail.com>
2025-09-16 07:27:23 -0700

    in files path provided some link

--- my-app/app/files/[[...filePath]]/page.js
+++ my-app/app/files/[[...filePath]]/page.js
@@ -10,6 +10,14 @@ export default async function File({ params }) {
     <div>
       <h1>Catch All Routes</h1>
       <p>The file path is {filePath}</p>
+      <p>You can access any file in the files directory.</p>
+      <p>For example:</p>
+      <ul>
+        <li>/files/documents/report.pdf</li>
+        <li>/files/images/photo.jpg</li>
+        <li>/files/videos/movie.mp4</li>
+        <li>/files/anything/you/want/to/access.txt</li>
+      </ul>
     </div>
   );
 }
