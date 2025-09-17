039136e04d75b6e9026f6b9fa17e64502c2325f7
coder8820 <kumail112113@gmail.com>
2025-09-17 11:06:52 -0700

    private folder in next js

---
+++ my-app/app/_components/page.js
@@ -0,0 +1,7 @@
+export default function ComponentPage() {
+  return (
+    <div>
+      <h1>This is my Component page</h1>
+    </div>
+  );
+}
---
+++ my-app/app/_components/test.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function test() {
+  return (
+    <div>
+      <h1>Test component page</h1>
+    </div>
+  );
+}
