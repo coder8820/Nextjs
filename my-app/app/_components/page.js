127710c95dbd0ab9d5c595ca6af2413fb9c29699
coder8820 <kumail112113@gmail.com>
2025-09-19 11:39:59 -0700

    some paragraph adde on pages files

--- my-app/app/_components/page.js
+++ my-app/app/_components/page.js
@@ -2,6 +2,32 @@ export default function ComponentPage() {
   return (
     <div>
       <h1>This is my Component page</h1>
+      <p>This is a reusable component page.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam itaque
+        quis odio excepturi. Et nobis quis tempore architecto fuga reiciendis
+        quod, soluta ut rem, tenetur incidunt iusto cum accusantium deleniti,
+        repellendus veritatis inventore at delectus. Accusamus pariatur
+        quibusdam iusto ex, corrupti nam. Est excepturi numquam molestias.
+        Corporis similique obcaecati eaque autem adipisci voluptatum fuga nam,
+        repellat doloribus corrupti id earum veritatis odit ab. Rerum nisi
+        veritatis cumque, quam at fugiat voluptatum sunt expedita! Earum totam
+        porro laboriosam eos dolore corporis asperiores, minus alias. Sed
+        doloremque accusamus culpa perspiciatis ut praesentium magnam commodi
+        mollitia dolores, expedita, dicta velit laboriosam aliquid? Porro
+        asperiores aspernatur saepe deserunt magni quas placeat earum fugit
+        laboriosam fugiat? Odio eveniet facere voluptatibus possimus quidem
+        iusto non, quisquam qui odit voluptatem aperiam atque sed sapiente
+        architecto molestiae, minima adipisci aspernatur impedit eaque in velit
+        vitae labore dicta? Earum tempora corrupti, doloremque aliquid
+        exercitationem cum repudiandae adipisci, quos iste aperiam perferendis
+        assumenda rerum. Eius asperiores quasi harum? Atque aliquam explicabo
+        beatae repellendus, cumque hic modi quisquam quaerat excepturi
+        exercitationem voluptate, labore ut eius pariatur delectus qui repellat.
+        Ducimus voluptatem distinctio debitis ipsa ad velit assumenda numquam
+        suscipit dicta. Porro expedita accusamus eius tempore quos quod sapiente
+        vitae dolor. Dignissimos.
+      </p>
     </div>
   );
 }
