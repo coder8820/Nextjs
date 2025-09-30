533816805bf8a5433041ec06e67e4589858a5fea
coder8820 <kumail112113@gmail.com>
2025-09-30 11:01:37 -0700

    i learned static site generation

--- my-app/app/(applications)/blogs/page.js
+++ my-app/app/(applications)/blogs/page.js
@@ -10,7 +10,12 @@ export async function generateMetadata({ params }) {
 }
 
 export async function generateStaticParams() {
-  return [{ blogID: "blog1" }, { blogID: "blog2" }, { blogID: "blog3" }];
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const data = await response.json();
+  console.log("data:", data);
+  return data.slice(0, 4).map((blog) => ({ blogID: `blog${blog.id}` }));
+
+  // return [{ blogID: "blog1" }, { blogID: "blog2" }, { blogID: "blog3" }];
 }
 
 export default function Blogs() {
