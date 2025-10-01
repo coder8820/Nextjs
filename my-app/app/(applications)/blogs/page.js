8946e2b664376e774fe10efa6ebeb507826b3874
coder8820 <kumail112113@gmail.com>
2025-10-01 10:08:02 -0700

    todays working stars

--- my-app/app/(applications)/blogs/page.js
+++ my-app/app/(applications)/blogs/page.js
@@ -24,7 +24,9 @@ export default function Blogs() {
       <div className="mx-auto p-4">
         <h1>Blogs Page</h1>
         <p>This is the blogs page.</p>
-        <h2>Blog 1</h2>
+        <Link href="blog1">
+          <h2>Blog 1</h2>
+        </Link>
         <h2>Blog 2</h2>
         <h2>Blog 3</h2>
         <h2>Blog 4</h2>
