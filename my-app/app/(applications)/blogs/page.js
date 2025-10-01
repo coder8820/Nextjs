50a38c0bec1d644756f2d6460569166e62eed772
coder8820 <kumail112113@gmail.com>
2025-10-01 13:12:11 -0700

    completed today practice

--- my-app/app/(applications)/blogs/page.js
+++ my-app/app/(applications)/blogs/page.js
@@ -9,6 +9,8 @@ export async function generateMetadata({ params }) {
   };
 }
 
+export const dynamicParams = false;
+
 export async function generateStaticParams() {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
   const data = await response.json();
