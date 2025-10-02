e6e22606138c4bf286977fde6acfb4d5edec5d81
coder8820 <kumail112113@gmail.com>
2025-10-02 10:44:53 -0700

    folder structure is ready

---
+++ e-store/src/app/store/product_details/[product_id]/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ProductDetails() {
+  return <div></div>;
+}
+
+const CategoryListing = () => {
+  return <div></div>;
+};
+
+const ProductListing = () => {
+  return <div></div>;
+};
