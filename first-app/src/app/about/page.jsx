a7966584a6d0d6081a3cb46358013aa85e2aa2b9
coder8820 <kumail112113@gmail.com>
2025-10-02 07:39:04 -0700

    added header page

--- first-app/src/app/about/page.jsx
+++ first-app/src/app/about/page.jsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 export default function about() {
   return (
-    <div>
-      <h1>about page</h1>
-      <button>
-        <Link href="/">go to home page</Link>
-      </button>
-    </div>
+    <main className="main">
+      <div>
+        <h1>about page</h1>
+        <button>
+          <Link href="/">go to home page</Link>
+        </button>
+      </div>
+    </main>
   );
 }
