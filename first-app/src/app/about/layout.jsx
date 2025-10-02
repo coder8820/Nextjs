9abb590d07f1328675c77d562831e451dc288ac3
coder8820 <kumail112113@gmail.com>
2025-10-02 08:11:00 -0700

    created layout of about and course page

---
+++ first-app/src/app/about/layout.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AboutLayout({ children }) {
+  return <div style={{ border: "2px solid green" }}>{children}</div>;
+}
---
+++ first-app/src/app/course/layout.jsx
@@ -0,0 +1,5 @@
+export default function courseLayout({ children }) {
+  return (
+    <div style={{ border: "1px solid red", padding: "1rem" }}>{children}</div>
+  );
+}
