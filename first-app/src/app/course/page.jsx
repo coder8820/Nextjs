11d217c469d945e81518ec58f68d59af61193a2a
coder8820 <kumail112113@gmail.com>
2025-10-02 08:34:31 -0700

    dealing with image

--- first-app/src/app/course/page.jsx
+++ first-app/src/app/course/page.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import image from "@/images/attitude.png";
+import Image from "next/image";
 
 export default function coursePage() {
   return (
     <main>
       <h1>These Courses are we offering!</h1>
+      <Image width={400} height={400} src={image.src} />
     </main>
   );
 }
