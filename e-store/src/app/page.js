0a8f96048b0f6c969b51118213238b575b2c2d3f
coder8820 <kumail112113@gmail.com>
2025-10-03 00:03:57 -0700

    Now working on hero section

--- e-store/src/app/page.js
+++ e-store/src/app/page.js
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/home/HeroSection";
+
 export default function Home() {
   return (
-    <div>
-      <h1>E-store</h1>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 }
--- e-store/src/components/home/HeroSection.jsx
+++ e-store/src/components/home/HeroSection.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Slider from "./Slider";
+import Container from "../Container";
+import styles from "@/styles/header.module.css";
 
 export default function HeroSection() {
   return (
-    <div>
+    <Container>
+      <section className={`${styles.hero}`}>
+        <div className={`${styles.textSection}`}>
+          <h1>
+            One stop solution <span className="text-pink-500">E-store</span>
+          </h1>
+          <p>Discover the latest headphones, earphones, mobiles tablets etc.</p>
+          <p>Exclusive details just for you!</p>
+          <button className={styles.ctaButton}>Shop Now</button>
+        </div>
+      </section>
       <Slider />
-    </div>
+    </Container>
   );
 }
