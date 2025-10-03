4d6c89291ef0132872d478fd2eb55de8077f7f13
coder8820 <kumail112113@gmail.com>
2025-10-02 22:59:18 -0700

    working on header of the web page

--- e-store/src/components/Header.jsx
+++ e-store/src/components/Header.jsx
@@ -1,10 +1,54 @@
-import React from "react";
 import Container from "./Container";
+import styles from "@/styles/header.module.css";
+import { FiSearch, FiShoppingCart } from "react-icons/fi";
 
 export default function Header() {
   return (
-    <header>
-      <Container></Container>
+    <header className={`${styles.header} py-3 px-1 shadow`}>
+      <Container>
+        <div className="flex item-center">
+          <span className="text-pink-500 font-bold text-4xl">
+            E-store<b className="text-black">.</b>
+          </span>
+        </div>
+        {/* Search Bar */}
+        <div className={`${styles.searchBar} flex items-center`}>
+          <input
+            type="text"
+            placeholder="Search for Prodcuts..."
+            className={`${styles.searchInput}`}
+          />
+          <button className={styles.searchButton}>
+            <FiSearch size={18} />
+          </button>
+        </div>
+        {/* Navigation bar */}
+        <NavBar />
+      </Container>
     </header>
   );
 }
+
+// navBar
+const NavBar = () => {
+  return (
+    <nav className="flex items-center gap-5">
+      {/* Navigation links */}
+      <ul className="flex items-center gap-3 font-semibold">
+        {/* import React from "react"; */}
+        <li className={styles.navLink}>Home</li>
+        <li className={styles.navLink}>store</li>
+      </ul>
+      {/* cart icon */}
+      <div className="relative">
+        <FiShoppingCart color="black" size={24} />
+        <span
+          className={`${styles.cartBadge} absolute top-[-15px] right-[-20px] bg-pink-500
+          text-white text-xs font-bold w-5 h-5 flex items-center justity-center rounded-full`}
+        >
+          3
+        </span>
+      </div>
+    </nav>
+  );
+};
