dd63a6eb2c0708edf523b6fc1be4bf005e40a3ab
coder8820 <kumail112113@gmail.com>
2025-10-02 23:28:26 -0700

    header is almost completed

--- e-store/src/components/Container.jsx
+++ e-store/src/components/Container.jsx
@@ -1,5 +1,5 @@
-export default function Container({ classname, children }) {
+export default function Container({ className, children }) {
   return (
-    <div className={`max-w-[1200px] mx-auto ${classname}`}>{children}</div>
+    <div className={`max-w-[1200px] mx-auto ${className}`}>{children}</div>
   );
 }
--- e-store/src/components/Header.jsx
+++ e-store/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { FiSearch, FiShoppingCart } from "react-icons/fi";
 export default function Header() {
   return (
     <header className={`${styles.header} py-3 px-1 shadow`}>
-      <Container>
+      <Container className="flex justify-between">
         <div className="flex item-center">
           <span className="text-pink-500 font-bold text-4xl">
             E-store<b className="text-black">.</b>
@@ -36,15 +36,15 @@ const NavBar = () => {
       {/* Navigation links */}
       <ul className="flex items-center gap-3 font-semibold">
         {/* import React from "react"; */}
-        <li className={styles.navLink}>Home</li>
-        <li className={styles.navLink}>store</li>
+        <li className={`${styles.navLink}`}>Home</li>
+        <li className={`${styles.navLink}`}>store</li>
       </ul>
       {/* cart icon */}
       <div className="relative">
-        <FiShoppingCart color="black" size={24} />
+        <FiShoppingCart className={styles.cart} color="black" size={24} />
         <span
           className={`${styles.cartBadge} absolute top-[-15px] right-[-20px] bg-pink-500
-          text-white text-xs font-bold w-5 h-5 flex items-center justity-center rounded-full`}
+          text-white text-xs font-bold w-5 h-5 flex items-center justity-between rounded-full`}
         >
           3
         </span>
