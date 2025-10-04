e6bb937ff565b9ac21039de5a698f0f1db4058da
coder8820 <kumail112113@gmail.com>
2025-10-04 10:14:07 -0700

    start working today

--- e-store/src/app/store/page.jsx
+++ e-store/src/app/store/page.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function page() {
-  return <div>store page</div>;
+  return (
+    <div>
+      <h1>Store page</h1>
+      <p>This is the store page</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
 }
