f7021326437078e039789e71e0c07b2cf681bb10
coder8820 <kumail112113@gmail.com>
2025-10-08 09:55:02 -0700

    map method used for categorylisting

--- e-store/src/app/store/product_details/[product_id]/page.jsx
+++ e-store/src/app/store/product_details/[product_id]/page.jsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import React from "react";
+import { getCategories, getProducts } from "@/styles/Library";
 
 export default function StorePage() {
   return (
@@ -12,10 +13,28 @@ export default function StorePage() {
   );
 }
 
-const CategoryListing = () => {
-  return <div></div>;
+const CategoryListing = async () => {
+  const categories = await getCategories();
+  console.log(categories);
+  return (
+    <div>
+      <div className="col-span-1 border-r-2 pr-5">
+        <h2 className="font-bold text-2xl mb-5">Categories</h2>
+        <ul className="space-y-3">
+          {categories.map((category) => (
+            <li key={category} className="cursor-pointer hover:underline">
+              {category}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-const ProductListing = () => {
+const ProductListing = async () => {
+  const products = await getProducts();
+  console.log(products);
+
   return <div className="col-span-4"></div>;
 };
