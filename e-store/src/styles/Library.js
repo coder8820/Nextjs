f91283c80b77aa5169f4677727a8eb37f87a7c4b
coder8820 <kumail112113@gmail.com>
2025-10-08 09:47:45 -0700

    we made to method to get product details

--- e-store/src/styles/Library.js
+++ e-store/src/styles/Library.js
@@ -1,5 +1,13 @@
 const getCategories = async () => {
   const res = await fetch("https://fakestoreapi.com/products/categories");
   const data = await res.json();
+  return data.categories;
+};
+
+const getProducts = async () => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
   return data;
 };
+
+export { getCategories, getProducts };
