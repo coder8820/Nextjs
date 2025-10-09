cef1a114204160eabf098a6aee9d84154873983b
coder8820 <kumail112113@gmail.com>
2025-10-08 23:24:12 -0700

    everything is clear now

--- e-store/src/styles/Library.js
+++ e-store/src/styles/Library.js
@@ -1,6 +1,6 @@
 const getCategories = async () => {
   try {
-    const res = await fetch("https://fakestoreapi.com/products/categorie");
+    const res = await fetch("https://fakestoreapi.com/products/categories");
     if (!res.ok) throw new Error("Failed to fetch categories");
     const data = await res.json();
     return data;
