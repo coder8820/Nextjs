cdbf269d28f1b8e152c28851e0dc9fcb8d6112bf
coder8820 <kumail112113@gmail.com>
2025-10-15 11:07:50 -0700

    some project deleted

--- nlp_app/frontend/app/layout.js
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="bg-gray-50 text-gray-900">
-        <main className="max-w-4xl mx-auto p-6">{children}</main>
-      </body>
-    </html>
-  );
-}
