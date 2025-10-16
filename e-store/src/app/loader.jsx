da1fe9c2ca18c801cbb884bd1ffb5f74beeb551b
coder8820 <kumail112113@gmail.com>
2025-10-16 13:07:56 -0700

    something new here

--- e-store/src/app/loader.jsx
+++ e-store/src/app/loader.jsx
@@ -1,7 +1,9 @@
 export default function Loading() {
   return (
     <div className="flex justify-center items-center h-screen w-full bg-gray-100 ">
-      <h1 className="animate-spin h-16 w-16 rounded-full border-4 border-blue-400 border-b-transparent"></h1>
+      <h1 className="animate-spin h-16 w-16 rounded-full border-4 border-blue-400 border-b-transparent">
+        loading
+      </h1>
     </div>
   );
 }
